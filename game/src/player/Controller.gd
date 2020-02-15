751f86d24ac00f1bea90ff4831ccd36ad2b64cda
Robert Beckett <robert.beckett@onion.red>
2020-02-15 00:00:59 -0800

    Added basic controls to component and created signals for it to trigger other components.  Starting to mash components together in a parent Player container.

--- game/src/player/Controller.gd
+++ game/src/player/Controller.gd
@@ -1,16 +1,25 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+signal move_left
+signal move_right
+signal jump
+signal attack
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# ui_{left, right, up, down}
+
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		emit_signal("move_left")
+	if event.is_action_pressed("ui_right"):
+		emit_signal("move_right")
+	if event.is_action_pressed("ui_up"):
+		emit_signal("jump")
+	if event.is_action_pressed("ui_down"):
+		return
+	if event.is_action_pressed("attack"):
+		emit_signal("attack")
