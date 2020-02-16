2cf68eef049fdd345b2705a565a46025f06e34d0
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-16 10:05:19 -0800

    added randomness to idle and patrol timer

--- game/src/NPC/Bat.gd
+++ game/src/NPC/Bat.gd
@@ -81,7 +81,7 @@ func change_direction(direction_change):
 	
 func _on_Patrol_timeout():
 	speed = 0
-	$Timers/Idle.set_wait_time(rand_range(0, 2))
+	$Timers/Idle.set_wait_time(rand_range(0, 1.5))
 	$Timers/Idle.start()
 	
 
