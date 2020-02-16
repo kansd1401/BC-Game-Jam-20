c801f67de73328a8092c8b2a16eec3cdc97aec4e
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-15 21:54:22 -0800

    fixed collision on x axis on bat

--- game/src/NPC/Bat.gd
+++ game/src/NPC/Bat.gd
@@ -1,53 +1,71 @@
 extends KinematicBody2D
 
-
+var velocity = Vector2()
+var speed = 0
+var GRAVITY = 10
+var direction = Vector2(1, 1)
+var flipped = false
+var hp = 100
+var engaged = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var motion = Vector2()
-var last_input = ""
-var in_animation = false
+
+
 # Called when the node enters the scene tree for the first time.
-onready	var animation = $BatAnimation/AnimationPlayer
+func _ready():
+	$Timers/Idle.start()
 
-func _physics_process(delta):
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = speed * direction.x 
+	velocity.y = speed * direction.y
 	
-	if in_animation == false:
-		animation.play("flight")
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -100
-		last_input = "move_left"
-		animation.flip_h = true
-	elif Input.is_action_pressed("ui_right"):
-		motion.x = 100
-		last_input = "move_right"
-		animation.flip_h = false
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = -100
-		last_input = "move_up"
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = 100
-		last_input = "move_down"
-	elif Input.is_action_pressed("attack"):
-		animation.stop()
-		animation.play("attack")
-#		in_animation = true
-#		while in_animation == true:
-#			print(animation.get_animation())
-#			if animation.get_animation() != "attack":
-#
-#				animation.play("flight")
-#				in_animation = false
-		motion.y = 0
-		motion.x = 0
-		last_input = "attack"
 
+	
+	$BatAnimation/Flight.show()
+	$BatAnimation/Attack.hide()
+	$BatAnimation/Death.hide()
+	$BatAnimation/AnimationPlayer.play("Flight")
+	
+	if $Rays/FeetCollision.is_colliding():
+		print("feet touched something")
+		change_direction("feet")
+#	
+	if $Rays/ForwardCollision.is_colliding():
+		print("wall")
+		change_direction("forward")
+	if $Rays/HeadCollision.is_colliding():
+		print("watch your head")
+		change_direction("head")
+	move_and_slide(velocity)
+
+func change_direction(direction_change):
+	if direction_change == "forward":
+		direction.x = direction.x * -1
+		$Rays/ForwardCollision.position.x *= -1
+		$Rays/ForwardCollision.cast_to.x *= -1
+		$Rays/HeadCollision.position.x *= -1
+		$Rays/FeetCollision.position.x *= -1
+		velocity.x = speed * direction.x
+		move_and_slide(velocity)
 	else:
-		if last_input != "":
-#			animation.play("flight")
-			last_input = ""
-			
-#	if animation.is_playing() == false:
-#		in_animation = false
-#		animation.play("flight")
-	move_and_slide(motion)
+		direction.y = direction.y * -1
+		print("change directions not forward")
+		
+	if direction.x == 1:
+		flipped = false
+	else:
+		flipped = true
+	$BatAnimation/Attack.flip_h = flipped
+	$BatAnimation/Death.flip_h = flipped
+	$BatAnimation/Flight.flip_h = flipped
+	
+func _on_Patrol_timeout():
+	speed = 0
+	$Timers/Idle.start()
+
+
+func _on_Idle_timeout():
+	speed = 30
+	$Timers/Patrol.start()
