e160755e1eb404b14c10ae0d2efe18b61c058df1
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-15 18:26:15 -0800

    duno why im pulling

--- game/src/NPC/Bat.gd
+++ game/src/NPC/Bat.gd
@@ -8,7 +8,7 @@ var motion = Vector2()
 var last_input = ""
 var in_animation = false
 # Called when the node enters the scene tree for the first time.
-onready	var animation = $Sprite
+onready	var animation = $BatAnimation/AnimationPlayer
 
 func _physics_process(delta):
 	
@@ -16,33 +16,35 @@ func _physics_process(delta):
 		animation.play("flight")
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -100
-		last_input = ""
+		last_input = "move_left"
 		animation.flip_h = true
 	elif Input.is_action_pressed("ui_right"):
 		motion.x = 100
-		last_input = ""
+		last_input = "move_right"
 		animation.flip_h = false
 	elif Input.is_action_pressed("ui_up"):
 		motion.y = -100
-		last_input = ""
+		last_input = "move_up"
 	elif Input.is_action_pressed("ui_down"):
 		motion.y = 100
-		last_input = ""
+		last_input = "move_down"
 	elif Input.is_action_pressed("attack"):
 		animation.stop()
 		animation.play("attack")
-		in_animation = true
-		while in_animation == true:
-			if animation.playing == false:
-				animation.play("flight")
-				in_animation = false
+#		in_animation = true
+#		while in_animation == true:
+#			print(animation.get_animation())
+#			if animation.get_animation() != "attack":
+#
+#				animation.play("flight")
+#				in_animation = false
 		motion.y = 0
 		motion.x = 0
-		last_input = ""
+		last_input = "attack"
 
 	else:
 		if last_input != "":
-			animation.play("idle")
+#			animation.play("flight")
 			last_input = ""
 			
 #	if animation.is_playing() == false:
