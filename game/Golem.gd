ca0243c74ee03a7b42fb91f4d5a7f073f768f671
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-15 17:21:19 -0800

    golem walks on direction now

---
+++ game/Golem.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var SPEED = 30
+var GRAVITY = 10
+var direction = 1
+var flipped = false
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = SPEED * direction
+	velocity.y += GRAVITY
+	if velocity.x == 0:
+		$GolemAnimation/IdleE.show()
+		$GolemAnimation/AnimationPlayer.play("IdleE")
+	else:
+		$GolemAnimation/Walk.show()
+		$GolemAnimation/IdleE.hide()
+		$GolemAnimation/Attack.hide()
+		$GolemAnimation/AnimationPlayer.play("Walk")
+	if !$Rays/EdgeDetector.is_colliding():
+		print("edge")
+		change_direction()
+	else:
+		velocity.y = 0
+	if $Rays/WallDetector.is_colliding():
+		print("wall")
+		change_direction()
+	move_and_slide(velocity)
+
+func change_direction():
+	direction = direction*-1
+	$Rays/EdgeDetector.position.x *= -1
+	$Rays/WallDetector.position.x *= -1
+	$Rays/WallDetector.cast_to.x *= -1
+	if direction == 1:
+		flipped = false
+	else:
+		flipped = true
+	$GolemAnimation/Attack.flip_h = flipped
+	$GolemAnimation/Death.flip_h = flipped
+	$GolemAnimation/Idle.flip_h = flipped
+	$GolemAnimation/IdleE.flip_h = flipped
+	$GolemAnimation/Walk.flip_h = flipped
+
+
+func _on_Timer_timeout():
+	pass
