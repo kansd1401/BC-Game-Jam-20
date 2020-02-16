543da54fb5aa41753b0dcc720d25eb872912bef3
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-16 10:02:20 -0800

    fixed a bug where rays arent properly changing directions

--- game/src/NPC/Bat.gd
+++ game/src/NPC/Bat.gd
@@ -21,24 +21,15 @@ func _ready():
 func _process(delta):
 	velocity.x = speed * direction.x 
 	velocity.y = speed * direction.y
-	print(direction.x)
-	$Rays/ForwardCollision.position.x *= direction.x
-	$Rays/ForwardCollision.cast_to.x = direction.x
-	$Rays/HeadCollision.position.x *= direction.x
-	$Rays/FeetCollision.position.x *= direction.x
-
+	
 	bat_location = get_position()
-	print(bat_location.y)
 	if bat_location.y < 350:
 		direction.y = 1
-#
-
 	
 	$BatAnimation/Flight.show()
 	$BatAnimation/Attack.hide()
 	$BatAnimation/Death.hide()
 	$BatAnimation/AnimationPlayer.play("Flight")
-	
 		
 	if $Rays/FeetCollision.is_colliding():
 		print("feet touched something")
@@ -47,21 +38,36 @@ func _process(delta):
 	if $Rays/ForwardCollision.is_colliding():
 		print("wall")
 		change_direction("forward")
+		
 	if $Rays/HeadCollision.is_colliding():
 		print("watch your head")
 		change_direction("head")
+		
 	move_and_slide(velocity)
 
 func change_direction(direction_change):
-	if direction_change == "forward":
-		direction.x = direction.x * -1
-		velocity.x = speed * direction.x
-		move_and_slide(velocity)
+	print("WE CHANGING DIRECTIONS")
+	if direction_change == "forward" && direction.x == 1:
+		direction.x = -1
+#		velocity.x = speed * direction.x
+#		move_and_slide(velocity)
+		$Rays/ForwardCollision.position.x *= direction.x
+		$Rays/ForwardCollision.cast_to.x *= direction.x
+		$Rays/HeadCollision.position.x *= direction.x
+		$Rays/FeetCollision.position.x *= direction.x
+	elif direction_change == "forward" && direction.x == -1:
+#		velocity.x = speed * direction.x
+#		move_and_slide(velocity)
+		$Rays/ForwardCollision.position.x *= direction.x
+		$Rays/ForwardCollision.cast_to.x *= direction.x
+		$Rays/HeadCollision.position.x *= direction.x
+		$Rays/FeetCollision.position.x *= direction.x
+		direction.x = 1
 	elif direction_change == "feet":
 		direction.y = -1
 	else:
 		direction.y = 1
-		
+	
 		
 	if direction.x == 1:
 		flipped = false
@@ -71,26 +77,43 @@ func change_direction(direction_change):
 	$BatAnimation/Death.flip_h = flipped
 	$BatAnimation/Flight.flip_h = flipped
 	
+	print("why changing X on feet collision", direction.x)
+	
 func _on_Patrol_timeout():
 	speed = 0
+	$Timers/Idle.set_wait_time(rand_range(0, 2))
 	$Timers/Idle.start()
 	
 
 
 func _on_Idle_timeout():
 	speed = 50
-	if randi() == 1:
+	randomize()
+	if randi()%2 == 1:
 		direction.x = 1
 		print("changing direction to the right")
+		if $Rays/ForwardCollision.position.x < 0:
+			$Rays/ForwardCollision.position.x *= -1
+			$Rays/ForwardCollision.cast_to.x *= -1
+			$Rays/HeadCollision.position.x *= -1
+			$Rays/FeetCollision.position.x *= -1
 	else:
 		direction.x = -1
-		print("changing direction to the left")
+		print("changing direction to the left") 
+		print(direction.x, "DEBUG THIS")
+		if $Rays/ForwardCollision.position.x > 0:
+			$Rays/ForwardCollision.position.x *= direction.x
+			$Rays/ForwardCollision.cast_to.x *= direction.x
+			$Rays/HeadCollision.position.x *= direction.x
+			$Rays/FeetCollision.position.x *= direction.x
 
-	if randi() == 1:
+	if randi()%2 == 1:
 		direction.y = 1
 		print("changing direction to the down")
 	else:
 		direction.y = -1
 		print("changing direction to the up")
-#
+
+
+	$Timers/Patrol.set_wait_time(rand_range(1, 4))
 	$Timers/Patrol.start()
